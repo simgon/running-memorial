e6e3a34a7f931d2924bfa108db78ee9caf91c24d
simgon <procar1000@yahoo.co.jp>
2023-08-20 10:56:30 +0000

    訂正

--- db/migrate/20230820102615_add_not_null_constraint.rb
+++ db/migrate/20230820102615_add_not_null_constraint.rb
@@ -1,6 +1,7 @@
 class AddNotNullConstraint < ActiveRecord::Migration[7.0]
   def change
     change_column :users, :user_token, :string, null: false
-    change_column :routes, :user_id, :string, null: false
+    remove_reference :routes, :user
+    add_reference :routes, :user, null: false, foreign_key: true
   end
 end
