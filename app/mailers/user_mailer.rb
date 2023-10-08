b813da914575888f69f462a07598b80643cd64fc
simgon <procar1000@yahoo.co.jp>
2023-10-08 00:31:59 +0000

    確認メールの内容を変更

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "アカウントを有効化"
+    mail to: user.email, subject: "[ランニング・メモリアル]アカウントを有効化"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,6 +17,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "パスワードを再設定"
+    mail to: user.email, subject: "[ランニング・メモリアル]パスワードを再設定"
   end
 end
