0d1add2c9a9ddf32c0782a91f92bbf61512ffe5d
simgon <procar1000@yahoo.co.jp>
2023-10-15 03:55:23 +0000

    プライバシーポリシーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   delete "/logout",         to: "sessions#destroy"
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  get    "/privacypolicy",  to: "static_pages#privacypolicy"
 end
